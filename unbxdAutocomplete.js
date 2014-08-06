da6403b146e1dedb853233b37e3a987fe37e7e63
Praveen <praveen@unbxd.com>
2014-08-06 11:32:37 +0530

    check if price present before making it fixed

--- unbxdAutocomplete.js
+++ unbxdAutocomplete.js
@@ -865,7 +865,7 @@ function myAjax(openCallback) {
 						}else if(products[k].doctype ===  "POPULAR_PRODUCTS" ){
 							var imageUrl 	= _CONST.popularProducts.imageUrl;  
 							obj.imgUrl 		= 	products[k][ imageUrl ] ? products[k][ imageUrl ] : products[k].image_url;
-							obj.price 		= 	_CONST.popularProducts.currency+products[k].price.toFixed(2);
+							obj.price 		= 	products[k].price ? _CONST.popularProducts.currency+products[k].price.toFixed(2) : "";
 							obj.uniqueId 	=  	products[k].uniqueId;
 							obj.isProduct 	= 	true;
 							obj.productUrl	=   products[k][_CONST.popularProducts.productUrl]
